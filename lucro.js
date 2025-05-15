2da75801992bceae7053a466c78fd10f8c9c3aad
SERGIO HENRIQUE DA SILVA <26528@unifacef.edu.br>
2025-05-15 20:42:53 -0300

    new commit

---
+++ lucro.js
@@ -0,0 +1,19 @@
+function lucro(){
+    let preco = 5,0
+    let qtd = 120
+    const despesa = 200
+    let lucro
+    aux = ""
+    while (preco >=1){
+        lucro = (preco*qtd) - despesa
+        alert(`${preco} - ${qtd} - ${despesa} - ${lucro}`)
+        qtd = qtd + 26
+        preco = preco * 0.5
+
+    }
+
+alert(aux)
+
+}
+
+// calcular maior lucro maior despezqa e maior preço
