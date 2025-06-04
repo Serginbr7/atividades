546b087d569acb111cce3bb3eb58d8bccb3db8df
SERGIO HENRIQUE DA SILVA <26528@unifacef.edu.br>
2025-06-03 22:07:46 -0300

    atividades sobre vetor em js

---
+++ vetor.js
@@ -0,0 +1,17 @@
+function vetor(){
+    let vet = []
+
+    for(let i=0; i<10; i++){
+        vet.push(number(prompt(`Informe o ${i+1} elemento`)))
+    }
+    let soma = 0
+    for( let i=0. i<10; i++){
+        soma = soma + vet
+    }
+    alert(`A media dos eleentos é $(soma/vet.lenght)`)
+}
+
+//programa em js que solicite 6 números
+//calcular e exiir:
+//os numeros oares e a qtd de pares
+//os numeros impares e a qtde de impares
