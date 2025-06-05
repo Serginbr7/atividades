c6b5e1094bd9bc0094b5e8bda8f9680be7f0e4df
SERGIO HENRIQUE DA SILVA <26528@unifacef.edu.br>
2025-06-05 20:41:16 -0300

    vetor atividades

---
+++ exe3.js
@@ -0,0 +1,9 @@
+function exe3(){
+
+    let vet = []
+    let o1 =0, o2 = 0, o3 = 0
+   for(let i=0; i<10; i++){
+    vet[i] = Number(prompt(`Digite o seu voto, 1, 2, 3`))
+   } 
+   for(let i=0;)
+}
--- vetor.js
+++ vetor.js
@@ -5,14 +5,14 @@ function vetor(){
         vet.push(number(prompt(`Informe o ${i+1} elemento`)))
     }
     let soma = 0
-    for( let i=0. i<10; i++){
+    for( let i=0; i<10; i++){
         soma = soma + vet
     }
     alert(`A media dos eleentos é $(soma/vet.lenght)`)
 }
 
 //programa em js que solicite 6 números
-//calcular e exiir:
+//calcular e exibir:
 //os numeros oares e a qtd de pares
 //os numeros impares e a qtde de impares
 
@@ -20,3 +20,44 @@ function vetor(){
 //calcule e mostre o betor alterado da seguinte forma:
 //nas posições pares, comar 10 no elemento
 //nas posições ímpares, multiplicar por 3 o elemento
+
+function programa6numeros() {
+    let vet = []
+    let contpar = 0, contimpar
+    for(let i=0; i<6; i++){
+        vet.push(number(prompt(`Informe o ${i + 1} elemento`)))
+    }
+    
+    let pares = []
+    let impares = []
+
+    for(let i=0; i<6; i++){
+        if(vet[i] %2 == 0){
+        pares.push(vet[i])
+        contpar ++
+        }
+    
+        else{ impares.push(vet[i])
+            contimpar ++
+        }    
+    }
+    alert(`Existem ${impares} números ímpares: ${}`)
+}
+
+function parimpar(){
+    let vet = []
+    for(let i=0; i<10; i++){
+        vet.push(Number(prompt(`Informe o ${i+1} número`)))
+    }
+    
+    for(let i=0; i<10; i++){
+        if (vet[i] %2 == 0){
+            vet[i] += 10
+        }
+        else{
+            vet[i] *= 3
+        }
+        
+    }
+    alert(`Novo vetor alterado ${vet}`)
+}
